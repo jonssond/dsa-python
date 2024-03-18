7dfc0190211b0b52ab3710f21210782834427608
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-03-18 20:42:19 -0300

    append and pop

--- linked-lists/ll-constructor.py
+++ linked-lists/ll-constructor.py
@@ -3,7 +3,6 @@ class Node:
         self.value = value
         self.next = None
 
-
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
@@ -11,6 +10,34 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-my_linked_list = LinkedList(4)
-print(my_linked_list.head.value)
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else: 
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        else: 
+            temp, pre = self.head
+            while (temp.next):
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.print_list()
